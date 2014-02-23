91fdcc12f9e261ed3097eb3607992f5b2e6bae92
ryancstack <ryancstack@me.com>
2014-02-22 18:57:12 -0800

    integrating. adding game over

---
+++ Protect Your Jewels/Protect Your Jewels/RSGameOver.m
@@ -0,0 +1,56 @@
+//
+//  RSGameOver.m
+//  Operation: Protect Your Jewels
+//
+//  Created by Ryan Stack on 2/22/14.
+//  Copyright (c) 2014 ITP382RBBM. All rights reserved.
+//
+
+#import "RSGameOver.h"
+#import "RSTempGamePlayScene.h"
+#import "RSMainMenuLayer.h"
+
+@implementation RSGameOver
+
++ (CCScene *)scene {
+    CCScene *scene = [CCScene node];
+	RSGameOver *layer = [RSGameOver node];
+	[scene addChild: layer];
+	return scene;
+}
+
+-(id) init
+{
+	if( (self=[super init]) )
+	{
+		CGSize size = [[CCDirector sharedDirector] winSize];
+		
+		[CCMenuItemFont setFontSize:28];
+        [CCMenuItemFont setFontName:@"Helvetica"];
+		
+		// Achievement Menu Item using blocks
+		CCMenuItem *itemNewGame = [CCMenuItemFont itemWithString:@"Try Again" block:^(id sender) {
+            
+            [[CCDirector sharedDirector] replaceScene:[CCTransitionFade transitionWithDuration:0.5 scene:[RSTempGamePlayScene node]]];
+			
+		}];
+        CCMenuItem *itemMainMenu = [CCMenuItemFont itemWithString:@"Quit" block:^(id sender) {
+            
+            [[CCDirector sharedDirector] replaceScene:[CCTransitionFade transitionWithDuration:0.5 scene:[RSMainMenuLayer node]]];
+			
+		}];
+		
+		CCMenu *menu = [CCMenu menuWithItems:itemNewGame, itemMainMenu, nil];
+		
+		[menu alignItemsVerticallyWithPadding:10];
+        [menu setPosition:ccp(size.width/2, 100)];
+		
+		// Add the menu to the layer
+		[self addChild:menu];
+        
+	}
+	return self;
+}
+
+
+@end
